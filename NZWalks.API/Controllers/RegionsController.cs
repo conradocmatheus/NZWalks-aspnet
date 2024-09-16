a8a0c506b6926fd9f80a3ed1d924029164c04ef2
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-16 14:46:02 -0300

    Controllers and DTOs with some tests

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,97 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:****/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NzWalksDbContext _dbContext;
+
+        public RegionsController(NzWalksDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Get data from DB - Domain Models
+            var regionsDomain = _dbContext.Regions.ToList();
+
+            // Map Domain Models to DTOs
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto
+                    {
+                        Id = regionDomain.Id,
+                        Code = regionDomain.Code,
+                        Name = regionDomain.Name,
+                        RegionImageUrl = regionDomain.RegionImageUrl
+                    }
+                );
+            }
+
+            // Return DTOs
+            return Ok(regionsDto);
+        }
+
+        // GET REGION BY ID
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = _dbContext.Regions.Find(id);
+
+            var regionDomain = _dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomain == null)
+            {
+                return NotFound("Region was not found");
+            }
+
+            // Map Region Domain Model to region DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+        }
+
+        [HttpPut]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            // Map DTO to Domain Model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            _dbContext.Regions.Add(regionDomainModel);
+            _dbContext.SaveChanges();
+
+            // Map Domain Model back to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id}, regionDto);
+        }
+    }
+}
---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Mark", "Emely", "David" };
+            return Ok(studentNames);
+        }
+    }
+}
