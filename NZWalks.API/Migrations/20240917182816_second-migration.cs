8cba6d3906dc426ccb121c3e2c574f2caf098a6c
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-17 15:29:25 -0300

    migration rest

---
+++ NZWalks.API/Migrations/20240917182816_second-migration.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class secondmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Difficulties",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Difficulties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RegionImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Walks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LenghtInKm = table.Column<double>(type: "float", nullable: false),
+                    WalkImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DifficultyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RegionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Walks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Walks_Difficulties_DifficultyId",
+                        column: x => x.DifficultyId,
+                        principalTable: "Difficulties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Walks_Regions_RegionId",
+                        column: x => x.RegionId,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Difficulties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("19f369b0-2d0d-4aa1-956a-6d26f713f3bb"), "Medium" },
+                    { new Guid("5298b2f7-0ff4-4e0b-8b9f-cc82ca635f89"), "Hard" },
+                    { new Guid("cf46cf69-e0fa-4c03-9c31-7418618fb203"), "Easy" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageUrl" },
+                values: new object[,]
+                {
+                    { new Guid("05bbedfd-7509-4c81-b495-9c3f3b6c6360"), "TRG", "Tauranga", "https://picsum.photos/204" },
+                    { new Guid("7bded420-749a-426f-9d18-99e3b9b7ff87"), "WLG", "Wellington", "https://picsum.photos/201" },
+                    { new Guid("8c1b0f4b-5470-4fa6-98f1-25c4321dc64e"), "HAM", "Hamilton", "https://picsum.photos/203" },
+                    { new Guid("c2f1f0da-34a8-4b5d-b8ff-fb00e1a332d4"), "AKL", "Auckland", "https://picsum.photos/200" },
+                    { new Guid("fbec72b6-b80a-44b1-bbaa-3ab21f4ffcd7"), "CHC", "Christchurch", "https://picsum.photos/202" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_DifficultyId",
+                table: "Walks",
+                column: "DifficultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_RegionId",
+                table: "Walks",
+                column: "RegionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Walks");
+
+            migrationBuilder.DropTable(
+                name: "Difficulties");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+        }
+    }
+}
